85ca83e202421f367b7a9abd4811a218045eabd6
Sonukumarsha <sonukumarsha9113@gmail.com>
2025-03-24 11:50:19 +0530

    updated

--- App.jsx
+++ App.jsx
@@ -1,9 +1,18 @@
 import React, {useEffect} from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
-import SmartechBaseReact from 'smartech-base-react-native'
+import SmartechBaseReact from 'smartech-base-react-native';
 
 const App = () => {
+  useEffect(() => {
+    SmartechBaseReact.setUserIdentity("swayam146", (result, error) => {
+      if (error) {
+        console.error("Failed to set user identity", error);
+      } else {
+        console.log(result);
+      }
+    })
+  }, []);
   return (
     <SafeAreaProvider>
       <AppNavigator />
